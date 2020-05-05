f2eaa33b3cb931a76c91517b8fbc906ef3821398
Rafael Garcia <rafagrcb@gmail.com>
2020-05-05 22:11:23 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,8 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_language(Dan, Emberjs)
+def introduction_with_language(name, language)
+  name = Dan
+  language = Ember.js
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
